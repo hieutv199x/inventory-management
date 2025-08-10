58ecc7a78cc58eb8f352ae51db56f5a37a048c29
LongTran <tranlong2002.tt@gmail.com>
2025-08-10 15:47:38 +0700

    update code

--- src/app/api/tiktok/refresh-token/route.ts
+++ src/app/api/tiktok/refresh-token/route.ts
@@ -6,42 +6,46 @@ const prisma = new PrismaClient();
 
 export async function GET(req: Request) {
   try {
-    const { app_key } = await req.json();
+    const { shopId } = await req.json();
 
-    const credential = await prisma.tikTokAppCredential.findFirst({
-      where: { appKey: app_key },
+    const shop = await prisma.shopAuthorization.findFirst({
+      where: { shopId: shopId },
+        include:{
+          app: true
+        }
   });
 
-    if (!credential) {
+    if (!shop) {
       return NextResponse.json({ error: "Missing required parameters" }, { status: 400 });
     }
-    const appSecret = credential.appSecret;
+    const appSecret = shop.app.appSecret;
 
     if (!appSecret) {
         return NextResponse.json({ error: 'App secret is missing in database' }, { status: 500 });
     }
-    const refresh_token = credential.refreshToken
+
+    const refresh_token = shop.refreshToken
 
     if (!refresh_token) {
       return NextResponse.json({ error: 'App secret is missing in database' }, { status: 500 });
   }
 
-    const { body } = await AccessTokenTool.refreshToken(refresh_token, app_key, appSecret);  
+    const { body } = await AccessTokenTool.refreshToken(refresh_token, shop.app.appKey, appSecret);
         console.log('getAccessToken resp data := ', JSON.stringify(body, null, 2));  
         const parsedBody = typeof body === 'string' ? JSON.parse(body) : body;
         const data = parsedBody.data;
         if (!data?.access_token) {
           throw new Error('Failed to retrieve access token from TikTok');
       }
-      await prisma.tikTokAppCredential.update({
-          where: { id: credential.id },
+      await prisma.shopAuthorization.update({
+          where: { id: shop.id },
           data: {
               accessToken: data.access_token,
               refreshToken: data.refresh_token,
               expiresIn: (data as any).access_token_expire_in,
               scope: Array.isArray((data as any).granted_scopes) ? (data as any).granted_scopes.join(',') : (data as any).granted_scopes,
               updatedAt: new Date(),
-              status: 'active',
+              status: 'ACTIVE',
           },
       });
 
