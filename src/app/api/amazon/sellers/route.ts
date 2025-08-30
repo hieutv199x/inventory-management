2cce40bbd0a20a4d6001cb55b26662bec22c76a5
dragon <duybd1@viettel.com.vn>
2025-08-30 07:47:48 +0700

    add amazon sdk

--- src/app/api/amazon/sellers/route.ts
+++ src/app/api/amazon/sellers/route.ts
@@ -1,16 +1,19 @@
-import { sellersService } from '@/lib/amazon-sp-api';
 import { NextResponse } from 'next/server';
+import { SellersApi, ApiClient as SellersApiClient } from '../../../../amazon_sdk/sellers/index';
+import {AppConfig} from '../../../../../app.config.mjs';
 
 export const runtime = 'nodejs';
 
 export async function GET() {
   try {
-    console.log("Getting marketplace participations");
-    const data = await sellersService.getMarketplaceParticipations();
-    return NextResponse.json(data);
+    const sellerApiClient = new SellersApiClient(AppConfig.endpoint);
+    const sellerApi = new SellersApi(sellerApiClient);
+    sellerApiClient.enableAutoRetrievalAccessToken(AppConfig.lwaClientId, AppConfig.lwaClientSecret, AppConfig.lwaRefreshToken);
+    const participations = await sellerApi.getMarketplaceParticipations();
+    return NextResponse.json(participations);
   } catch (error) {
     console.error("Sellers API error:", error);
-    return NextResponse.json({ 
+    return NextResponse.json({
       error: (error as Error).message,
       stack: (error as Error).stack
     }, { status: 500 });
