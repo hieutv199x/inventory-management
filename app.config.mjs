d8427e81b1264f2024517ed7eae731ff2c40f40d
LongTran <tranlong2002.tt@gmail.com>
2025-09-01 12:10:28 +0700

    Commit

--- app.config.mjs
+++ app.config.mjs
@@ -1,10 +1,9 @@
-export const AppConfig = {
-  endpoint: 'https://sandbox.sellingpartnerapi-na.amazon.com',
-  spApiNAEndpoint: 'https://sandbox.sellingpartnerapi-na.amazon.com',
-  lwaClientId: 'amzn1.application-oa2-client.5714646b8477491bb2f7bf0c82bc7276',
-  lwaClientSecret: 'amzn1.oa2-cs.v1.456d9253348800513a872b2ba0481687113ecee20f5fb2ed2f47bd1f816f65ef',
-  lwaRefreshToken: 'Atzr|IwEBILG0Sl6tX-Wi6XA0N4r6MreYqss_3wgSz3LvjEOaY_LYLqVCIUiPFLLl2Rn7AYXj_sYBRZwxJKTbqUFgWB-odYgY1MzflDR6t_VEteei-AC2SMQK0CiVAIQrnyg8w2iRsnEApXJ_rnpCkQnZpsBXfKCbhmrj2scHP92NCh91AHAdf9dHzt7holaZQCK60FA2IqeokxZl00sy81jEii7m3YGELvNQu3vtYMrDv21goNUZpxWxt5oPwjaAgbynquBHJX3WP48Lv3h1JnfAZHV9WunFpkF4IRrTymu5Qrz3JwrQamY_o088ancwKkfuTrU1hI4',
-  // Optional AWS credentials for signing requests (if required)
-  // awsAccessKey: 'your-aws-access-key',
-  // awsSecretKey: 'your-aws-secret-key'
-};
+// export const AppConfig = {
+//   spApiNAEndpoint: 'https://sandbox.sellingpartnerapi-na.amazon.com',
+//   lwaClientId: 'amzn1.application-oa2-client.ffc6d63acaad44068977bcc3c77d2c2c',
+//   lwaClientSecret: 'amzn1.oa2-cs.v1.f364fd06e6823340eb2328cf86b3d6062687a92e9c8386fe2b8c3bd52ee67ac7',
+//   lwaRefreshToken: 'Atzr|IwEBIMw9Xfhzohwfq1shHbjTmeC4-CNvwFq_PjI9EnSU-fEpR2TmlWmTq81lBHr868YG79qe22DhHBTxB6tfmMqcuoxaABDfxqnhOkc_nuehHFmX7QOcIIonVZYIP1qcqZyrRWg4Ttni_tFDHZR74yRAYvpFoPVeA-XYypPCsazJ3HbDbW6zcfHoGSAus4Jj5PUXNnXVB5XTIZFud7ThNBWLHl4y4oQXzOz_xYEAtDHwgwnhQA20zlDQZvwn6AUwmLXmlbeAY-8Kegniz8PXNMsbsXEIAduU2YTksjKpfQjeXe9gzSp-qOGty1zrkdk6LkBkNho',
+//   // Optional AWS credentials for signing requests (if required)
+//   // awsAccessKey: 'your-aws-access-key',
+//   // awsSecretKey: 'your-aws-secret-key'
+// };
