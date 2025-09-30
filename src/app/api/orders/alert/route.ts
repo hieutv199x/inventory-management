3a0397decf98daa391c3e26f0fafd1c1a54e39ed
dragon <duybd1@viettel.com.vn>
2025-09-30 14:43:15 +0700

    Commit

--- src/app/api/orders/alert/route.ts
+++ src/app/api/orders/alert/route.ts
@@ -54,14 +54,14 @@ export async function GET(req: NextRequest) {
                         shippingDueTime: { gt: now, lt: twentyFourHoursFromNow },
                         status: { notIn: ["AWAITING_COLLECTION", "IN_TRANSIT", "DELIVERED", "COMPLETED", "CANCELLED"] }
                     },
-                    {
-                        collectionDueTime: { gt: now, lt: twentyFourHoursFromNow },
-                        status: { notIn: ["IN_TRANSIT", "DELIVERED", "COMPLETED", "CANCELLED"] }
-                    },
-                    {
-                        deliveryDueTime: { gt: now, lt: twentyFourHoursFromNow },
-                        status: { notIn: ["DELIVERED", "COMPLETED", "CANCELLED"] }
-                    }
+                    // {
+                    //     collectionDueTime: { gt: now, lt: twentyFourHoursFromNow },
+                    //     status: { notIn: ["IN_TRANSIT", "DELIVERED", "COMPLETED", "CANCELLED"] }
+                    // },
+                    // {
+                    //     deliveryDueTime: { gt: now, lt: twentyFourHoursFromNow },
+                    //     status: { notIn: ["DELIVERED", "COMPLETED", "CANCELLED"] }
+                    // }
                 ]
             }
         });
